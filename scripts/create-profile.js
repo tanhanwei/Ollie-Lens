77e0c0504c432d5e58b314a424ead2dff0b5dfb2
Kenny Meyer <kenny@kennymeyer.net>
2022-03-23 23:55:00 -0300

    add scripts

---
+++ scripts/create-profile.js
@@ -0,0 +1,37 @@
+import { apolloClient } from './apollo-client';
+// this is showing you how you use it with react for example
+// if your using node or something else you can import using
+// @apollo/client/core!
+import { gql } from '@apollo/client'
+
+const request = {
+    
+  handle: "testthw",
+  profilePictureUri: null,   
+  followModule: {
+        emptyFollowModule: true
+    } 
+}
+
+const CREATE_PROFILE = `
+  mutation($request: CreateProfileRequest!) { 
+    createProfile(request: $request) {
+      ... on RelayerResult {
+        txHash
+      }
+      ... on RelayError {
+        reason
+      }
+            __typename
+    }
+ }
+`
+
+export const createProfile = (createProfileRequest) => {
+   return apolloClient.mutate({
+    mutation: gql(CREATE_PROFILE),
+    variables: {
+      request: request
+    },
+  })
+}
